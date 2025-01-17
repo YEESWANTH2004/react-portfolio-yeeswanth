c8d3d61fc326e3ddf6f364ac913688bcda584f19
Yeeswanth <yeesw@YeeswanthLatha.com>
2025-01-17 09:58:09 +0530

    Portfolio completed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,34 +1,52 @@
-import { CONTACT } from "../constants"
-import { motion } from "motion/react"
+import { CONTACT } from "../constants";
+import { motion } from "motion/react";
 
 const Contact = () => {
   return (
-    <div className="border-b border-neutral-900 pb-24 ">
-      <motion.h1 
-      whileInView={{opacity: 1, y:0}}
-      initial={{opacity:0 ,y:-100}}
-      transition={{duration:0.5}}
-       className="my-20 text-center text-4xl">Get In Touch</motion.h1>
+    <div className="border-b border-neutral-900 pb-24">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Get In Touch
+      </motion.h1>
       <div className="text-center">
-        <motion.p 
-        whileInView={{opacity: 1, x:0}}
-        initial={{opacity:0 ,x:-100}}
-        transition={{duration:1}}
-        className="my-4">{CONTACT.address}</motion.p>
-        <motion.p 
-        whileInView={{opacity: 1, x:0}}
-        initial={{opacity:0 ,x:100}}
-        transition={{duration:1}} 
-        className="my-4">{CONTACT.phoneNo}</motion.p>
-        <a href={
-          `mailto:${CONTACT.email}`} 
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.address}
+        </motion.p>
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="my-4"
+        >
+          {CONTACT.phoneNo}
+        </motion.p>
+        <a
+          href={`mailto:${CONTACT.email}`}
           className="border-b hover:text-blue-500"
           aria-label={`Email me at ${CONTACT.email}`}
         >
-          {CONTACT.email}</a>
+          {CONTACT.email}
+        </a>
+        <motion.p
+          whileInView={{ opacity: 1, y: 0 }}
+          initial={{ opacity: 0, y: 100 }}
+          transition={{ duration: 1 }}
+          className="mt-36 my-8 text-lg font-bold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 bg-clip-text text-transparent"
+        >
+          Created by Yeeswanth P L
+        </motion.p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
